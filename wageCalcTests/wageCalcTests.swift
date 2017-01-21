1c343abe5806ed1574200a240cf2f8e4af5e1807
Oliver Mulcahy <om@eduroam-169-233-242-121.ucsc.edu>
2017-01-21 03:45:59 -0800

    Initial Commit

---
+++ wageCalcTests/wageCalcTests.swift
@@ -0,0 +1,36 @@
+//
+//  wageCalcTests.swift
+//  wageCalcTests
+//
+//  Created by Oliver Mulcahy on 1/21/17.
+//  Copyright © 2017 Oliver Mulcahy. All rights reserved.
+//
+
+import XCTest
+@testable import wageCalc
+
+class wageCalcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
